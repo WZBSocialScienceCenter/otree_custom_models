a32462f32ae961d80fd6b4e67dc8d905431b3748
Markus Konrad <markus.konrad@wzb.eu>
2016-10-28 15:23:02 +0200

    initial commit - basic decisions example app

---
+++ example_decisions/models.py
@@ -0,0 +1,63 @@
+import random
+
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+from otree.db.models import Model, ForeignKey
+
+
+author = 'Markus Konrad'
+
+doc = """
+Custom models example: Arbitrary number of complex decisions per player.
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'example_decisions'
+    players_per_group = None
+    num_rounds = 2
+    num_decisions_per_round = 5
+
+
+class Subsession(BaseSubsession):
+    def before_session_starts(self):   # called each round
+        """For each player, create a fixed number of "decision stubs" with random values to be decided upon later."""
+        for p in self.get_players():
+            p.generate_decision_stubs()
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    def generate_decision_stubs(self):
+        """
+        Create a fixed number of "decision stubs", i.e. decision objects that only have a random "value" field on
+        which the player will base her or his decision later in the game.
+        """
+        for _ in range(Constants.num_decisions_per_round):
+            decision = self.decision_set.create()    # create a new Decision object as part of the player's decision set
+            decision.value = random.randint(1, 10)
+            decision.save()   # important: save to DB!
+
+
+class Decision(Model):   # our custom model inherits from Django's base class "Model"
+    REASONS = (
+        ('dont_know', "Don't know"),
+        ('example_reason', "Example reason"),
+        ('another_example_reason', "Another example reason"),
+    )
+
+    value = models.IntegerField()    # will be randomly generated
+    boolean_decision = models.BooleanField()
+    reason = models.CharField(choices=REASONS)
+
+    player = ForeignKey(Player)    # creates 1:m relation -> this decision was made by a certain player
+
+    def __str__(self):
+        return 'decision #%d for player %d (participant %d) - value %d'\
+               % (self.pk, self.player.pk, self.player.participant.id_in_session, self.value)
---
+++ example_decisions/views.py
@@ -0,0 +1,58 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants, Player, Decision
+from django.forms import modelformset_factory
+
+
+DecisionFormSet = modelformset_factory(Decision, fields=('boolean_decision', 'reason'), extra=0)
+
+
+class MakeDecisionsPage(Page):
+    def vars_for_template(self):
+        decision_qs = Decision.objects.filter(player__exact=self.player)
+        assert len(decision_qs) == Constants.num_decisions_per_round
+
+        decisions_formset = DecisionFormSet(queryset=decision_qs)
+
+        return {
+            'decision_formset': decisions_formset,
+            'decision_values_and_forms': zip([dec.value for dec in decision_qs], decisions_formset.forms),
+        }
+
+    def before_next_page(self):
+        # get the raw submitted data as dict
+        submitted_data = self.form.data
+
+        # get all decisions belonging to this player and save as dict with decision ID lookup
+        decision_objs_by_id = {dec.pk: dec for dec in self.player.decision_set.all()}
+        assert len(decision_objs_by_id) == Constants.num_decisions_per_round
+
+        for i in range(Constants.num_decisions_per_round):
+            input_prefix = 'form-%d-' % i
+
+            # get the inputs
+            dec_id = int(submitted_data[input_prefix + 'id'])
+            bool_decision = submitted_data[input_prefix + 'boolean_decision']
+            reason = submitted_data[input_prefix + 'reason']
+
+            # lookup by ID and save submitted data
+            dec = decision_objs_by_id[dec_id]
+
+            if bool_decision != '':
+                dec.boolean_decision = bool_decision == 'True'
+            else:
+                dec.boolean_decision = None
+
+            if reason != '':
+                dec.reason = reason
+            else:
+                dec.reason = None
+
+            # important: save to DB!
+            dec.save()
+
+
+page_sequence = [
+    MakeDecisionsPage,
+]
